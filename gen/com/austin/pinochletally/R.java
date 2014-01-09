d088e446ca430e99fbc9d6db508f45f2e34ccc78
Austin Sharp <austinsharp111@gmail.com>
2014-01-08 23:05:07 -0800

    empty shell of navigation drawer added

--- gen/com/austin/pinochletally/R.java
+++ gen/com/austin/pinochletally/R.java
@@ -9,12 +9,13 @@ package com.austin.pinochletally;
 
 public final class R {
     public static final class array {
-        public static final int meld_values=0x7f060001;
-        public static final int melds=0x7f060000;
-        public static final int trick_alt_values=0x7f060005;
-        public static final int trick_values=0x7f060003;
-        public static final int tricks=0x7f060002;
-        public static final int tricks_alt=0x7f060004;
+        public static final int game_types=0x7f060000;
+        public static final int meld_values=0x7f060002;
+        public static final int melds=0x7f060001;
+        public static final int trick_alt_values=0x7f060006;
+        public static final int trick_values=0x7f060004;
+        public static final int tricks=0x7f060003;
+        public static final int tricks_alt=0x7f060005;
     }
     public static final class attr {
     }
@@ -29,41 +30,46 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int drawer_shadow=0x7f020000;
+        public static final int ic_drawer=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090018;
-        public static final int add_scores=0x7f09000c;
-        public static final int bid=0x7f09000e;
-        public static final int bidding_team=0x7f090010;
-        public static final int error_msg=0x7f09000d;
-        public static final int fragment_game=0x7f090001;
-        public static final int fragment_scoring=0x7f090013;
-        public static final int fragment_scoring_melds=0x7f090014;
-        public static final int fragment_scoring_tricks=0x7f090017;
-        public static final int fragment_scoring_tricks_simple=0x7f090016;
-        public static final int history_grid=0x7f090011;
-        public static final int pager=0x7f090000;
-        public static final int section_label=0x7f090012;
-        public static final int switch_scoring_button=0x7f090015;
-        public static final int team1_list=0x7f090003;
-        public static final int team1_meld=0x7f090005;
-        public static final int team1_score=0x7f090004;
-        public static final int team1_tricks=0x7f090006;
-        public static final int team1name=0x7f090002;
-        public static final int team2_list=0x7f090008;
-        public static final int team2_meld=0x7f09000a;
-        public static final int team2_score=0x7f090009;
-        public static final int team2_tricks=0x7f09000b;
-        public static final int team2name=0x7f090007;
-        public static final int trump=0x7f09000f;
+        public static final int action_settings=0x7f09001a;
+        public static final int add_scores=0x7f09000e;
+        public static final int bid=0x7f090010;
+        public static final int bidding_team=0x7f090012;
+        public static final int drawer_layout=0x7f090000;
+        public static final int error_msg=0x7f09000f;
+        public static final int fragment_game=0x7f090003;
+        public static final int fragment_scoring=0x7f090015;
+        public static final int fragment_scoring_melds=0x7f090016;
+        public static final int fragment_scoring_tricks=0x7f090019;
+        public static final int fragment_scoring_tricks_simple=0x7f090018;
+        public static final int history_grid=0x7f090013;
+        public static final int left_drawer=0x7f090002;
+        public static final int pager=0x7f090001;
+        public static final int section_label=0x7f090014;
+        public static final int switch_scoring_button=0x7f090017;
+        public static final int team1_list=0x7f090005;
+        public static final int team1_meld=0x7f090007;
+        public static final int team1_score=0x7f090006;
+        public static final int team1_tricks=0x7f090008;
+        public static final int team1name=0x7f090004;
+        public static final int team2_list=0x7f09000a;
+        public static final int team2_meld=0x7f09000c;
+        public static final int team2_score=0x7f09000b;
+        public static final int team2_tricks=0x7f09000d;
+        public static final int team2name=0x7f090009;
+        public static final int trump=0x7f090011;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_game=0x7f030001;
-        public static final int fragment_history=0x7f030002;
-        public static final int fragment_main_dummy=0x7f030003;
-        public static final int fragment_scoring=0x7f030004;
+        public static final int drawer_list_item=0x7f030001;
+        public static final int fragment_game=0x7f030002;
+        public static final int fragment_history=0x7f030003;
+        public static final int fragment_main_dummy=0x7f030004;
+        public static final int fragment_scoring=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f080000;
@@ -71,6 +77,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int drawer_close=0x7f050007;
+        public static final int drawer_open=0x7f050006;
         public static final int switch_button=0x7f050005;
         public static final int title_section1=0x7f050002;
         public static final int title_section2=0x7f050003;
--- src/com/austin/pinochletally/GameFragment.java
+++ src/com/austin/pinochletally/GameFragment.java
@@ -13,7 +13,6 @@ import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ToggleButton;
