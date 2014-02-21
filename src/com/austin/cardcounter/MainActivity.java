ee3ae3c544fdba510aa2ef5ea845cb30601ce66d
Austin Sharp <austinsharp111@gmail.com>
2014-02-21 14:58:57 -0800

    500 scoring page done

--- src/com/austin/cardcounter/MainActivity.java
+++ src/com/austin/cardcounter/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
